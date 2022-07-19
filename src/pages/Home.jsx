3a9b4595e1b25319b79c4a4e3962a1fc2757b70b
Frank Condezo <frankcondezo@gmail.com>
2022-07-19 09:09:19 -0500

    add folder pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+function Home() {
+  return (
+    <Box>
+      <Heading>Welcome to Chaos</Heading>
+    </Box>
+  );
+}
+
+export default Home;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+function Login() {
+  return (
+    <Box>
+      <Heading>Welcome to Chaos</Heading>
+    </Box>
+  );
+}
+
+export default Login;
