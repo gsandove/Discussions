2794e5ae03732c8a5bab658ccf0531acc12515ea
Frank Condezo <frankcondezo@gmail.com>
2022-07-19 09:08:09 -0500

    add example of how to use css with chacra-ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,40 @@
+import { Box, ChakraProvider, Heading, Link } from "@chakra-ui/react";
+
+const linkStyles = {
+  color: "#ffffffcc",
+  transition: "color 0.15s",
+  lineHeight: "24px",
+  _hover: { color: "#ffffff" },
+};
+
 function App() {
-  return <div>Hi!</div>;
+  return (
+    <ChakraProvider>
+      {/* move this to navbar component */}
+      <Box as="nav" background="#ff6600" p="16px" mb="32px" width="100%">
+        <Box
+          color="white"
+          display="flex"
+          margin="auto"
+          maxW="920px"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Link href="/" {...linkStyles}>
+            Chaos news
+          </Link>
+          <Link href="/login" {...linkStyles}>
+            Login
+          </Link>
+        </Box>
+      </Box>
+      {/* end navbar component */}
+
+      <Box maxW="920px" margin="auto">
+        <Heading>Welcome to Chaos</Heading>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
